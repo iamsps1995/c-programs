25ed4af8e37682a80c4f2d06e0bb011e56a43fda
iamsps1995 <iamsps1995>
2022-06-29 09:42:54 +0530

    this is my first version

---
+++ 1.c
@@ -0,0 +1,13 @@
+// power program 
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+ double base , exp, result ;
+ base = 2 ;
+ exp = 3 ;
+ result= pow(base,exp);
+ printf("%.2lf", result);
+
+
+}
