270623ff2f1df480ea93e81c5928d412ac7dd393
iamsps1995 <iamsps1995>
2022-07-01 10:39:06 +0530

    added

--- 6.c
+++ 6.c
@@ -1,24 +1,20 @@
-// Write a program in C to copy the elements of one array into another array.
-#include <stdio.h>
-int main()
-{
-    int i, n, a[10], b[10];
-    printf("enter array elements ");
-    for (size_t i = 0; i <= 10; i++)
+//with arguements and return type
+#include<stdio.h>
+ int sum (int, int );
 
-    {
-        scanf("%d", &a[i]);
-    }
-    for (size_t i = 0; i <= 10; i++)
-    {
-        a[i] = b[i];
-    }
-    for (size_t i = 0; i <= 10; i++)
-    {
-        printf("%d", a[i]);
-    }
-    for (size_t i = 0; i <= 10; i++)
-    {
-        printf("%d", b[i]);
-    }
-}
+ void main()
+ {
+     int x , y , z;
+     printf("enter two numbers");
+     scanf("%d %d", &x , &y);
+     z=sum(x,y);
+     printf("sum=%d", z);
+
+ }
+ int sum(int a , int b)
+ {
+     int sum ;
+     sum=a+b;
+     return sum ;
+
+ }
